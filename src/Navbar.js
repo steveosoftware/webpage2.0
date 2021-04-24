cb3dcd667dcdd2146eb3811c0764575811f2656a
SteveO <stinkypiece@yahoo.com>
2021-04-23 20:56:26 -0700

    navbar links space out the way we want yay

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { NavLink } from 'react-router-dom';
+import headshot from './assets/images/headshot.jpg';
+import './Navbar.scss';
 
 class Navbar extends Component {
 
@@ -7,33 +9,41 @@ class Navbar extends Component {
 
     return(
 
-      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
-        <NavLink className='navbar-brand' to='/'>
-          SteveoSoftware
-        </NavLink>
-
-      <button
-        className='navbar-toggler'
-        type='button'
-        data-toggle='collapse'
-        data-target='#navbarNav'
-        aria-controls='navbarNav'
-        aria-expanded='false'
-        aria-label='Toggle navigation'
-      >
-        <span className='navbar-toggler-icon' />
-      </button>
-      <div className='collapse navbar-collapse' id='navbarNav'>
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <NavLink exact to='/' className='nav-link'>
-              Home
-            </NavLink>
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target=".dual-collapse2">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+    <div className="navbar-collapse collapse w-100 order-1 order-md-0 dual-collapse2">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item disable">
+          <NavLink className="nav-link brand" to="/">SteveoSoftware</NavLink>
           </li>
-            Projects
+
+            <li className="nav-item link home-link">
+                <NavLink className="nav-link" to="/">Home</NavLink>
+            </li>
+            <li className="nav-item link">
+                <NavLink exact to="/about" className="nav-link">About</NavLink>
+            </li>
+            <li className="nav-item link">
+                <NavLink className="nav-link" to="/projects">Projects</NavLink>
+            </li>
         </ul>
-      </div>
-    </nav>
+    </div>
+    <div className="mx-auto order-0">
+
+    </div>
+    <div class="navbar-collapse collapse w-100 order-3 dual-collapse2">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="https://github.com/steveosoftware">GitHub</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="https://www.linkedin.com/in/steve-oconnor-927a701bb/">LinkedIn</a>
+            </li>
+        </ul>
+    </div>
+</nav>
 
 
     )
